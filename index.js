6f98fe7d3b8f209984e971472e43373f86443c89
osamamalik234 <osamamalik9171@gmail.com>
2024-03-17 20:30:01 +0500

    new committed

--- index.js
+++ index.js
@@ -4,11 +4,8 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.get("/contacts", (req, res) => {
-  res.status(200).json({
-    message: "I am osama malik",
-  });
-});
+
+app.use("/contacts", require("./Router/router"));
 
 app.listen(port, () => {
   console.log(`Server is running on port no ${port}`);
