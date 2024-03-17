bcc86db2d5b42cd79369887b2de5fc04b1f029d5
osamamalik234 <osamamalik9171@gmail.com>
2024-03-17 15:56:22 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const dotevn = require("dotenv").config();
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.get("/contacts", (req, res) => {
+  res.status(200).json({
+    message: "server here bro",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port no ${port}`);
+});
+
